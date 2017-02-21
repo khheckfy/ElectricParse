aed865df6501c3ad6a5b4853d24cc258085ba9e0
khusainov ruslan <khusainovheckfy@gmail.com>
2017-02-21 16:56:51 +0700

    1

--- ElectricParse.BusinessLayer/ParserService.cs
+++ ElectricParse.BusinessLayer/ParserService.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using ElectricParse.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ElectricParse.BusinessLayer
 {
-    class ParserService
+    public class ParserService
     {
+        public List<Category> ParseCategories()
+        {
+            List<Category> categories = new List<Category>();
+
+
+
+            return categories;
+        }
+
+        #region private methods
+
+        string GetPageHtml(string link, WebProxy proxy = null)
+        {
+            WebClient client = new WebClient() { Encoding = Encoding.GetEncoding("windows-1251") };
+            System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            using (client)
+            {
+                return client.DownloadString(link);
+            }
+        }
+
+        #endregion
     }
 }
