d9948514cd0bb8e04a71d97d848607a76a838920
khusainov ruslan <khusainovheckfy@gmail.com>
2017-02-21 16:33:29 +0700

    1

---
+++ ElectricParse.Domain/Entities/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricParse.Domain.Entities
+{
+    public class Category
+    {
+        public Guid ID { private set; get; }
+        public string Name { private set; get; }
+        public string Url { set; get; }
+        public Guid? ParentCategoryId { set; get; }
+
+        public List<Product> Products { set; get; }
+
+        public Category(string name)
+        {
+            ID = Guid.NewGuid();
+            Name = name;
+            Products = new List<Product>();
+        }
+    }
+}
---
+++ ElectricParse.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricParse.Domain.Entities
+{
+    public class Product
+    {
+        public string Name { set; get; }
+        public string Url { set; get; }
+        public string ImageUrl { set; get; }
+    }
+}
